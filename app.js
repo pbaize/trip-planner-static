2635e46159c533300158d1af1f7dd801b6e7e5ed
Pierre Baize <pierrebaize@Pierres-MacBook-Pro.local>
2016-08-16 11:12:13 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const swig = require('swig')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+// const models = require('./models')
+
+const app = express()
+// require routers
+
+app.engine('html', swig.renderFile)
+swig.setDefaults({ cache: false })
+app.set('view engine', 'html')
+app.set('views', __dirname + '/views')
+
+app.use(morgan('dev'))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(express.static(__dirname + '/node_modules'))
+app.use(express.static(__dirname + '/public'))
+
+// router goes here
+
+app.listen(1337, function () {
+  console.log('listening on 1337')
+})
+
+app.use(function (err, req, res, next) {
+  console.error(err)
+  res.status(500).send(err.message)
+})
